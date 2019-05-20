2c02c4faf290f33efb1a614cec2f99e7f59897b4
nuclearpinguin <turbaszek@gmail.com>
2019-05-20 23:51:17 +0200

    tests: random solver

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,11 +1,39 @@
 import pandas as pd
 
-from app.solvers import make_plot_data, data_from_solution
-from app.solvers.random_solver import Output
+from app.solvers import make_plot_data
+from app.solvers.random_solver import Output, solve
 from app.solvers.city import City
 
 
-class TestPlotData:
+class TestSolver:
+    def test_random_solver(self):
+        paths = [
+            {'city_from': 'A', 'city_to': 'B', 'travel_time': 1},
+            {'city_from': 'B', 'city_to': 'C', 'travel_time': 1},
+        ]
+
+        cities = [
+            {'name': 'A', 'x': 0, 'y': 0, 'quantity': 10},
+            {'name': 'B', 'x': 1, 'y': 0, 'quantity': 10},
+            {'name': 'C', 'x': 2, 'y': 0, 'quantity': 10},
+        ]
+
+        cities = pd.DataFrame(cities, columns=['name', 'x', 'y', 'quantity'])
+        paths = pd.DataFrame(paths, columns=['city_from', 'city_to', 'travel_time'])
+
+        time = pd.DataFrame([{'time': 2}])
+
+        solution, edges = solve(cities, paths, time)
+
+        assert isinstance(solution, Output)
+        assert isinstance(edges, list)
+
+        assert len(solution.path) == 3
+        for c in solution.path:
+            assert isinstance(c, City)
+
+        assert solution.time_left == 0
+        assert solution.total == 30
 
     def test_new_data(self):
         paths = [
@@ -22,7 +50,7 @@ class TestPlotData:
         cities = pd.DataFrame(cities, columns=['name', 'x', 'y', 'quantity'])
         paths = pd.DataFrame(paths, columns=['city_from', 'city_to', 'travel_time'])
 
-        time = pd.DataFrame([{'time': 12}])
+        time = pd.DataFrame([{'time': 2}])
 
         solution, cts, edges = make_plot_data(cities, paths, time)
 
