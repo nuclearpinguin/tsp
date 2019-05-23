5c24f34d7bca812bf7fb7d053cd796679e83a36e
nuclearpinguin <turbaszek@gmail.com>
2019-05-23 21:10:24 +0200

    feat: exact solver + docs

--- app/solvers/__init__.py
+++ app/solvers/__init__.py
@@ -3,19 +3,41 @@ from typing import Union, Tuple
 
 from .city import City
 from .random_solver import solve, Output
+from .exact_solver import exact_solve
 
 
 def make_plot_data(cities: pd.DataFrame,
                    paths: pd.DataFrame,
                    time: pd.DataFrame,
                    simulations: int = 50,
-                   time_limit: int = 20):
+                   time_limit: int = 20,
+                   exact: bool = False):
+    """
+    Given an input, creates new solution and returns
+    data in format required by app.helpers.make_graph.
+
+    Parameters
+    ----------
+    cities - validated data frame with cities
+    paths - validated data frame with paths
+    time - validated data frame with time
+    simulations - number of simulations (only for random_solver)
+    time_limit - time limit for solving (in seconds)
+    exact - if True then exact_solver is being used
+
+    Returns
+    -------
+    Tuple[Output, list, list]
+    """
 
     cities['name'] = cities.name.apply(lambda x: str(x))
     paths['city_from'] = paths.city_from.apply(lambda x: str(x))
     paths['city_to'] = paths.city_to.apply(lambda x: str(x))
 
-    solution, selected_edges = solve(cities, paths, time, simulations, time_limit)
+    if exact:
+        solution, selected_edges = exact_solve(cities, paths, time, time_limit)
+    else:
+        solution, selected_edges = solve(cities, paths, time, simulations, time_limit)
 
     check = lambda fc, tc: ((fc, tc) in selected_edges) or ((tc, fc) in selected_edges)
 
@@ -29,6 +51,20 @@ def make_plot_data(cities: pd.DataFrame,
 def data_from_solution(cities: Union[pd.DataFrame, None],
                        paths: Union[pd.DataFrame, None],
                        solution: Output) -> Tuple[list, list]:
+    """
+    Utilises provided solution and additional info for recreating
+    data for app.helpers.make_graph.
+
+    Parameters
+    ----------
+    cities - validated data frame with cities
+    paths - validated data frame with paths
+    solution - Output representing a solution
+
+    Returns
+    -------
+    Tuple[list, list]
+    """
     if (cities is not None) and (paths is not None):
         cities['name'] = cities.name.apply(lambda x: str(x))
         paths['city_from'] = paths.city_from.apply(lambda x: str(x))
