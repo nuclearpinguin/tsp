fff108528658e21c9d2523ca7515b5857da00f78
Przemysław Szczeciński <przszczecinski@gmail.com>
2019-05-24 02:43:07 +0200

    Update City
    Adding comments

--- app/solvers/city.py
+++ app/solvers/city.py
@@ -2,10 +2,22 @@ import pandas as pd
 
 
 class City:
-    """ This class defines how city is represented. """
+    """
+    This class defines how city is represented.
+    """
 
     def __init__(self, name: str, x: int, y: int, value: int = 0) -> None:
-        """ Initializes attributes. """
+        """
+        Initializes attributes.
+
+        Parameters
+        ----------
+        name - name of the city
+        x - x coordinate
+        y - y coordinate
+        value - amount of goods for salesman to sale
+        """
+
         self.name = str(name)
         self.x = x
         self.y = y
@@ -14,10 +26,18 @@ class City:
         self.visited = False
 
     def set_neighbours(self, d) -> None:
+        """
+
+        Parameters
+        ----------
+        d - external dictionary, obtained by convert_to_dict function from random_solver
+        """
         self.neighbours = d.get(self.name, {})   # get the dictionary of pairs {neighbour:travel_time}
 
     def __str__(self) -> str:
-        """ Defines how print(object_name) is displayed. """
+        """
+        Defines how print(object_name) is displayed.
+        """
         return f"City: {self.name}\n" + \
                f"Coord: ({self.x}, {self.y})\n" + \
                f"Value: {self.value}\n" + \
